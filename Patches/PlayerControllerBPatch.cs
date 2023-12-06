b029436753abe66f2302b57d8901d5c0e265521e
vDalisay <andersenvincentr@gmail.com>
2023-12-06 22:05:28 +0100

    git init

---
+++ Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,16 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+//Make sure this class resides in the Patches folder
+namespace SkibidiCompany.Patches;
+[HarmonyPatch(typeof(PlayerControllerB))]
+internal class PlayerControllerBPatch
+{
+    //If method is public you can access it using: [HarmonyPatch(nameof({className}.{methodName}))]
+    //Tells the order of the function to run in a certain order
+    [HarmonyPatch("Update")]
+    [HarmonyPostfix]
+    static void InfiniteSprintPatch(ref float ___sprintMeter)
+    {
+        ___sprintMeter = 1f;
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace SkibidiCompany;
+
+public static class PluginInfo
+{
+    public const string PLUGIN_GUID = "VoidStudios.SkibidiMod";
+
+    public const string PLUGIN_NAME = "VoidStudios Mod";
+
+    public const string PLUGIN_VERSION = "0.0.0.1";
+}
